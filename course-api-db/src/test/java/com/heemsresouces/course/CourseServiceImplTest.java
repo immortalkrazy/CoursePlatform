3febe2e8787a797e5e43bb2ed5cde21293856a48
Faisal Ibrahim <faisal.ibrahim@gmail.com>
2022-02-05 18:13:54 -0500

    added some JUnit test cases

---
+++ course-api-db/src/test/java/com/heemsresouces/course/CourseServiceImplTest.java
@@ -0,0 +1,14 @@
+package com.heemsresouces.course;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CourseServiceImplTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
